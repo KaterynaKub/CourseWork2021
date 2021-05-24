eab1497333ac0f8999dbe241822b523622cf2e19
Kateryna Kubyshka <ekaterina.kubyshka@gmail.com>
2021-05-24 16:28:24 +0300

    Add IIndex interface and its implementation

---
+++ CourseWork2021/CourseWork2021/Models/Index.cs
@@ -0,0 +1,36 @@
+﻿using CourseWork2021.Interfaces;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourseWork2021
+{
+    class Index : IIndex
+    {
+        public ConcurrentDictionary<string, List<string>> indexDictionary { get; set; }
+        public delegate void AddOrUpdateValue(string key, string value);
+        public AddOrUpdateValue addValue;
+
+        public void AddKey(string word, string file)
+        {
+            addValue += AddValueToDictionary;
+           indexDictionary.AddOrUpdate(word,file,)
+        }
+
+        public string[] GetIndexFiles(string word)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddValueToDictionary(string key, string value)
+        {
+
+        }
+
+        private void UpdateValueInDictionatry(string key, string value)
+        {
+
+        }
+    }
+}
