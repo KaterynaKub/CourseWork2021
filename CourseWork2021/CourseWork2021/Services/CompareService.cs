3da5c1ffa3b46cc404aa8ad11d0a5c205e15f625
Kateryna Kubyshka <ekaterina.kubyshka@gmail.com>
2021-05-26 01:54:11 +0300

    Add static class CompareService for comparison of dictionaries

---
+++ CourseWork2021/CourseWork2021/Services/CompareService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseWork2021.Services
+{
+    public class CompareService
+    {
+        static public bool Compare()
+        {
+            IndexerService indexerServiceSerial = new(1);
+            IndexerService indexerServiceParallel = new(8);
+            indexerServiceSerial.RunThreadsAsync().Wait();
+            indexerServiceParallel.RunThreadsAsync().Wait();
+            ConcurrentDictionary<string, IEnumerable<string>> dictionarySerial = indexerServiceSerial.Index.IndexDictionary;
+            ConcurrentDictionary<string, IEnumerable<string>> dictionaryParallel = indexerServiceParallel.Index.IndexDictionary;
+            foreach (var (word, files) in dictionarySerial)
+            {
+
+                if (dictionaryParallel.TryGetValue(word, out IEnumerable<string> value))
+                {
+                    if (value.Except(files).Any() || files.Except(value).Any())
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
