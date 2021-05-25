ea986187e0dd2592ab156f8c6bb8b6483adcb229
Kateryna Kubyshka <ekaterina.kubyshka@gmail.com>
2021-05-26 01:52:13 +0300

    Add TimerService realization

--- CourseWork2021/CourseWork2021/Services/TimerService.cs
+++ CourseWork2021/CourseWork2021/Services/TimerService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,22 @@ namespace CourseWork2021.Services
 {
     class TimerService
     {
+        private Stopwatch timer;
 
+        public TimerService()
+        {
+            timer = new Stopwatch();
+        }
+
+        public void MeasureTimeParallelThreadsAsync(int countOfThreads)
+        {
+            GC.Collect();
+            timer = new Stopwatch();
+            timer.Start();
+            IndexerService IndexerService = new(countOfThreads);
+            IndexerService.RunThreadsAsync().Wait();
+            timer.Stop();
+            Console.WriteLine($"Time : {timer.ElapsedMilliseconds} miliseconds");
+        }
     }
 }
