5402469f6487e37cf769ab97e8297c5ccf50f30f
Kateryna Kubyshka <ekaterina.kubyshka@gmail.com>
2021-05-26 01:55:39 +0300

    Add time and equel results (for parallel and serial programs) tests

---
+++ CourseWork2021/CourseWork2021/Program.cs
@@ -0,0 +1,22 @@
+﻿using CourseWork2021.Services;
+using System;
+using System.Threading.Tasks;
+
+namespace CourseWork2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimerService timerService = new();
+            for (int i = 1; i <= 10; i++)
+            {
+                timerService.MeasureTimeParallelThreadsAsync(i);
+            }
+            CompareService compareService = new();
+            bool areEquel = CompareService.Compare();
+            Console.Write("Dictionaries are ");
+            Console.Write(areEquel ? "Equel" : "Not equel");
+        }
+    }
+}
